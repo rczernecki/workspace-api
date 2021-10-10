02fb94e2ef587e0bafc3d3091a6c44282272d872
Rafal Czernecki <rczernecki@hotmail.com>
2021-10-10 21:59:20 +0200

    Refactor place factory to use sequences

--- spec/factories/places.rb
+++ spec/factories/places.rb
@@ -2,10 +2,10 @@ require 'securerandom'
 
 FactoryBot.define do
   factory :place do
-    name { 'Example Name' }
+    sequence(:name) { |i| "Example Name #{i}" }
     lat { 51.10771151288517 }
     lon { 17.042357510153508 }
-    slug { SecureRandom.uuid[0..32] }
+    sequence(:slug) { |i| "example-slug-#{i}" }
     rating { 0 }
   end
 end
