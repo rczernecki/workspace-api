6ab9e164d5f6ab6f682cf23916ae23f2a0e0da78
Rafal Czernecki <rczernecki@hotmail.com>
2021-10-10 21:38:20 +0200

    Disable default SQL queries logging

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  # self.verbose_query_logs = true
-  self.logger = Logger.new(STDOUT) if defined?(ActiveRecord::Base)
+  # self.logger = Logger.new(STDOUT) if defined?(ActiveRecord::Base)
 end
