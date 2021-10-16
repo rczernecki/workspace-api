b03e27991bdcb6170b71d5eaa2cf2c95677b1d56
Rafal Czernecki <rczernecki@hotmail.com>
2021-10-16 19:29:12 +0200

    Add error serializer

---
+++ app/controllers/error_serializer.rb
@@ -0,0 +1,16 @@
+module ErrorSerializer
+
+  def ErrorSerializer.serialize(errors)
+    return if errors.nil?
+
+    json = {}
+    new_hash = errors.to_hash(true).map do |k, v|
+      v.map do |msg|
+        { id: k, title: msg }
+      end
+    end
+    json[:errors] = new_hash.flatten
+    json
+  end
+
+end
